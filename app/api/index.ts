40eae9b153c3f1ca1473106054fd016644ca2e8d
Rajeev <rajeev.cse04@gmail.com>
2019-01-21 17:55:43 +0530

    multiple endpoints ans mssql api

--- app/api/index.ts
+++ app/api/index.ts
@@ -2,6 +2,10 @@ import bodyParser from "body-parser";
 import chalk from "chalk";
 import cors from "cors";
 import express from "express";
+import jsonxml from "jsontoxml";
+import sql from "mssql";
+import xml from "xml";
+import converter, { Options } from "xml-js";
 import { CodeGenerator } from "../utility/codegenerator";
 
 const app = express();
@@ -10,6 +14,20 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+// connection config
+
+const config = {
+    user: "Rajeev",
+    password: "Oak1nd1a01",
+    server: "192.168.1.50", // You can use 'localhost\\instance' to connect to named instance
+    database: "SamEmpDB",
+
+    options: {
+        encrypt: false, // Use this if you're on Windows Azure
+    },
+};
+
+const pool = new sql.ConnectionPool(config);
 // create api
 app.post("/createapi", async (req, res) => {
     // your logic here
@@ -24,6 +42,48 @@ app.post("/createapi", async (req, res) => {
     }
 });
 
+// get json data using sql
+app.get("/getEmpDataAsJson", async (req, res) => {
+    // your logic here
+    try {
+        const conn = await pool.connect();
+        const request = new sql.Request(conn);
+        const result = await request.query(`select top 10 first_name, last_name, email, salary from dbo.employees;`);
+        console.log(result.recordset);
+        await conn.close();
+        res.status(200).send(result.recordset);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
+    }
+});
+
+// get xml data using sql
+app.get("/getEmpDataAsXml", async (req, res) => {
+    // your logic here
+    try {
+        const conn = await pool.connect();
+        const request = new sql.Request(conn);
+        const result = await request.query(`select top 10 first_name, last_name, email, salary from dbo.employees;`);
+        // console.log(result.recordset);
+        const options: Options.JS2XML = { compact: true, ignoreComment: true, spaces: 4, parentKey: "employee" };
+        // console.log({ employee: result.recordset });
+        // const data: string = converter.js2xml({ employee: result.recordset }, options);
+        // const data = jsonxml({ employee: result.recordset });
+        const example3 = [{
+            toys: [{ toy: "Transformers", name: "boy" }, { toy: "GI Joe", name: "boy" },
+            { toy: "He-man", name: "boy" }],
+        }];
+        const data = xml(example3, true);
+        console.log(data);
+        await conn.close();
+        res.set("Content-Type", "text/xml");
+        res.status(200).send(data);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
+    }
+});
 // start server
 app.listen(3000, () => {
     console.log(chalk.bgRed("server is started on localhost:3000"));
--- app/commands/index.ts
+++ app/commands/index.ts
@@ -1,5 +1,7 @@
 import find from "find-process";
 import cmd from "node-cmd";
+// tslint:disable-next-line:no-var-requires
+const terminate = require("terminate");
 
 class NodeCommand {
     static startService(wheretostart: string) {
@@ -11,18 +13,38 @@ class NodeCommand {
         }
     }
 
-    static killProcessByID(pid: number | undefined) {
+    static async killProcessByID(pid: number | undefined) {
         try {
             if (pid === undefined || pid === 0) {
                 return;
             }
-            cmd.run(`kill ${pid}`);
+            await cmd.run(`kill ${pid}`);
+            console.log(`killed pid ${pid}`);
         } catch (error) {
-            console.log(error);
+            console.error(error);
             // throw error;
         }
     }
 
+    static killTaskByPid = async (pid: number | undefined) => {
+        try {
+            if (pid === undefined || pid === 0 || (await find("pid", pid, true)).length === 0) {
+                console.log(`pid is ${pid}`);
+                return;
+            }
+            console.log(`pid is real ${pid}`);
+            await terminate(pid, (err: any) => {
+                if (err) { // you will get an error if you did not supply a valid process.pid
+                    console.error("Oopsy: " + err); // handle errors in your preferred way.
+                } else {
+                    console.log("done"); // terminating the Processes succeeded.
+                }
+            });
+        } catch (error) {
+            console.error(error);
+            // throw error;
+        }
+    }
     static getAllNodeProcess = async (): Promise<IProcessInfo[]> => {
         try {
             const c: IProcessInfo[] = await find("name", "node", true);
